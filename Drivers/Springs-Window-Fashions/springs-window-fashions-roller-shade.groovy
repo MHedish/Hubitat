952ff59065cfa83910a4d64409a594bde065626f
MHedish <57190961+MHedish@users.noreply.github.com>
2021-06-17 05:30:02 -0400

    Update springs-window-fashions-roller-shade.groovy
    Added stopPositionChange()

--- Drivers/Springs-Window-Fashions/springs-window-fashions-roller-shade.groovy
+++ Drivers/Springs-Window-Fashions/springs-window-fashions-roller-shade.groovy
@@ -1,25 +1,30 @@
 /**
- *  Springs Window Fashions - Roller Shade Driver
- *  Author: Marc Hedish / Hubitat: MHedish
- *  Date: 01/14/21
- *
- *  Copyright 2020 Marc Hedish
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
- *
- */
+*  Springs Window Fashions - Roller Shade Driver
+*  Author: Marc Hedish / Hubitat: MHedish
+*  Date: 06/17/21
+*
+*  Copyright 2021 Marc Hedish
+*
+*  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License. You may obtain a copy of the License at:
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+*  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+*  for the specific language governing permissions and limitations under the License.
+*
+*  https://paypal.me/MHedish
+*
+*  Changelog:
+* 
+*  20210617 -- Added stopPositionChange()
+*
+*/
  
 def setVersion(){
     state.name = "Springs Window Fashions Roller Shade"
-	state.version = "1.0.0"
+	state.version = "1.0.1"
 }
 
 metadata {
@@ -177,6 +182,11 @@ def setPosition(value, duration = null) {
 def stop() {
     logDebug "Stop command issued."
     zwave.switchMultilevelV3.switchMultilevelStopLevelChange().format()
+}
+
+def stopPositionChange() {
+    logDebug "StopPositionChange command issued."
+    stop()
 }
 
 def preset() {
